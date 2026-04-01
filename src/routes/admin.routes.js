9b8885c7eda50054f60db8a25698e9470263d7c1
AbiPasundan <muhamad11wildan4n@gmail.com>
2026-04-01 16:43:18 +0700

    feat: create swagger for admin routes function end point /admin/products & /admin/users

--- src/routes/admin.routes.js
+++ src/routes/admin.routes.js
@@ -4,7 +4,65 @@ import productsRouter from "#/routes/admin/products.routes.js";
 
 const adminRouter = Router()
 
+/**
+ * 
+ * @openapi
+ * /admin/users:
+ *   get:
+ *     tags:
+ *       - admin
+ *     parameters:
+ *       - in: query
+ *         name: page
+ *         schema:
+ *           type: integer
+ *           default: 1
+ *         description: The page number for pagination
+ *       - in: query
+ *         name: limit
+ *         schema:
+ *           type: integer
+ *           default: 5
+ *         description: The number of items to return per page
+ *     responses:
+ *       200:
+ *         description: Successful response
+ *       400:
+ *         description: Bad request
+ *       500:
+ *         description: Internal server error
+ * 
+*/
 adminRouter.use("/admin", userRouter)
+/**
+ * 
+ * @openapi
+ * /admin/products:
+ *   get:
+ *     tags:
+ *       - admin
+ *     parameters:
+ *       - in: query
+ *         name: page
+ *         schema:
+ *           type: integer
+ *           default: 1
+ *         description: The page number for pagination
+ *       - in: query
+ *         name: limit
+ *         schema:
+ *           type: integer
+ *           default: 5
+ *         description: The number of items to return per page
+ *     responses:
+ *       200:
+ *         description: Successful response
+ *       400:
+ *         description: Bad request
+ *       500:
+ *         description: Internal server error
+ * 
+*/
 adminRouter.use("/admin", productsRouter)
 
 export default adminRouter
