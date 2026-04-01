c23367c01639b0c99b881026344fb3f72bfb4eb2
AbiPasundan <muhamad11wildan4n@gmail.com>
2026-04-01 15:14:06 +0700

    fet: create controller for detailproduct

--- src/controllers/browseProduct.controller.js
+++ src/controllers/browseProduct.controller.js
@@ -9,3 +9,40 @@ export async function getBrowseController(req, res) {
         result: rows
     })
 }
+
+export async function getDetailProductController(req, res) {
+    const { id: idStr } = req.params;
+    const id = parseInt(idStr, 10);
+
+    if (isNaN(id)) {
+        return res.status(400).json({
+            success: false,
+            message: "Invalid product ID format",
+        });
+    }
+
+    try {
+        const { rows } = await browseProduct.detailProductModels(id);
+
+        if (!rows || rows.length === 0) {
+            return res.status(404).json({
+                success: false,
+                message: "Product not found",
+            });
+        }
+
+        res.status(200).json({
+            success: true,
+            message: "Product found",
+            result: rows
+        });
+
+    } catch (error) {
+        console.error("Database Error:", error);
+        res.status(500).json({
+            success: false,
+            message: "Internal server error",
+            result: error.message
+        });
+    }
+}
