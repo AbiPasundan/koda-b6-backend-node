b65d65de0ed54aab020340ea9242d987a81d53f6
AbiPasundan <muhamad11wildan4n@gmail.com>
2026-04-01 15:13:52 +0700

    feat: create models for detailproduct

--- src/models/browseProduct.models.js
+++ src/models/browseProduct.models.js
@@ -23,3 +23,46 @@ export async function browseProductModels() {
 
     return browseProduct
 }
+
+export async function detailProductModels(id) {
+    const queryDetailProductModels = `
+    SELECT
+		p.*,
+
+		d.discount_rate,
+		d.is_flash_sale,
+
+		(SELECT JSON_AGG(path)
+		FROM product_images
+		WHERE product_id = p.id) AS images,
+
+		(SELECT JSON_AGG(
+			JSON_BUILD_OBJECT(
+				'size_name', size_name,
+				'size_price', size_price
+			)
+		)
+		FROM product_size
+		WHERE product_id = p.id) AS sizes,
+
+		(SELECT JSON_AGG(
+			JSON_BUILD_OBJECT(
+				'variant_name', variant_name,
+				'add_price', add_price
+			)
+		)
+		FROM product_variant
+		WHERE product_id = p.id) AS variants,
+
+		(SELECT AVG(ratings)
+		FROM reviews
+		WHERE product_id = p.id) AS rating
+
+	FROM products p
+	LEFT JOIN discount d ON p.id = d.discount_id
+    WHERE p.id = $1;
+    `
+    const detailProduct = await query(queryDetailProductModels, [id])
+
+    return detailProduct
+}
