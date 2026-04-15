7046022cfa474ca8bc50cce6e51fecb823da9bb1
AbiPasundan <muhamad11wildan4n@gmail.com>
2026-04-15 17:59:35 +0700

    feat: add func 500 internal server error

--- src/helper/response.helper.js
+++ src/helper/response.helper.js
@@ -49,7 +49,31 @@ export function ResponseErr404(res, { errors, links = [] }) {
         type: "http://localhost:3000", // change when it depoloy to http://68.183.226.223:3001
         title: "Resource Not Found",
         status: 404,
-        detail: errors || "Resource yang Anda minta tidak ditemukan.",
+        detail: (errors?.message || errors || "404 Not Found"),
+        instance: res.req.originalUrl,
+        _links: links.reduce((acc, link) => {
+            acc[link.rel] = { href: link.href };
+            return acc;
+        }, {})
+    });
+}
+
+/**
+ * Send response error 500 with HATEOAS standar.
+ * 
+ * @param {Object} res - Objek response Express
+ * @param {Object} options - Detail error
+ * @param {Error|string} options.errors - Objek error.
+ * @param {Array} options.links - Array object link { rel, href }.
+ */
+export function ResponseErr500(res, { error, links = [] }) {
+    res.setHeader('Content-Type', 'application/problem+json');
+
+    return res.status(500).json({
+        type: "http://localhost:3000",
+        title: "Internal Server Error",
+        status: 500,
+        detail: (error?.message || error || "An unexpected error occurred while processing the request."),
         instance: res.req.originalUrl,
         _links: links.reduce((acc, link) => {
             acc[link.rel] = { href: link.href };
