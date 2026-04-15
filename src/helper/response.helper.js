fb59dd1a5dbeb1f353580e2afdf01700361528b7
AbiPasundan <muhamad11wildan4n@gmail.com>
2026-04-15 17:46:27 +0700

    feat: create func response helper 404 with hateoas standard

--- src/helper/response.helper.js
+++ src/helper/response.helper.js
@@ -1,10 +1,29 @@
-export default function ResponseOk(res, code, status, message, data) {
+/**
+ * Send response success with HATEOAS standar .
+ * 
+ * @param {Object} res - Objek response Express.
+ * @param {Number} code - HTTP Status Code.
+ * @param {String} message - Message success.
+ * @param {Object|Array} data - Data sending.
+ * @param {Array} links - List link navigasi { rel, method, href }.
+ */
+export default function ResponseOk(res, code = 200, message = "Success", data = null, links = []) {
+    const hypermediaLinks = links.reduce((acc, link) => {
+        acc[link.rel] = {
+            href: link.href,
+            method: link.method || "GET"
+        };
+        return acc;
+    }, {});
+
     res.status(code).json({
-        success: status,
+        success: true,
         message: message,
-        data,
-    })
+        data: data,
+        _links: hypermediaLinks
+    });
 }
+
 
 export function ResponseErr(res, { code, message, errors, links }) {
     res.status(code).json({
@@ -13,4 +32,28 @@ export function ResponseErr(res, { code, message, errors, links }) {
         errors,
         links
     })
+}
+
+/**
+ * Send response error 404 with HATEOAS standar.
+ * 
+ * @param {Object} res - Objek response Express
+ * @param {Object} options - Detail error
+ * @param {string|Array} options.errors - Detail error.
+ * @param {Array} options.links - Array objek link { rel, href }.
+ */
+export function ResponseErr404(res, { errors, links = [] }) {
+    res.setHeader('Content-Type', 'application/problem+json');
+
+    return res.status(404).json({
+        type: "http://localhost:3000", // change when it depoloy to http://68.183.226.223:3001
+        title: "Resource Not Found",
+        status: 404,
+        detail: errors || "Resource yang Anda minta tidak ditemukan.",
+        instance: res.req.originalUrl,
+        _links: links.reduce((acc, link) => {
+            acc[link.rel] = { href: link.href };
+            return acc;
+        }, {})
+    });
 }
