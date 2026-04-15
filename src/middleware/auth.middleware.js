6bb5b10b7425bb71d66f9f13beb168af118af01f
AbiPasundan <muhamad11wildan4n@gmail.com>
2026-04-15 17:04:14 +0700

    fix: fix middleware when it bandingkan

--- src/middleware/auth.middleware.js
+++ src/middleware/auth.middleware.js
@@ -27,20 +27,27 @@ export default function auth(requiredRole = null) {
         try {
             const payload = verifyToken(token)
             if (!payload) {
-                return res.status(403).json({
+                return res.status(401).json({
                     success: false,
                     message: "Invalid token",
                     result: null
                 })
             }
 
+            console.log("PAYLOAD:", payload)
+            console.log("REQUIRED ROLE:", requiredRole)
+
             res.locals.user = payload
-            if (requiredRole && payload.role_name !== requiredRole) {
-                return res.status(403).json({
-                    success: false,
-                    message: "Forbidden: insufficient role",
-                    result: null
-                })
+            if (requiredRole) {
+                const roles = [].concat(requiredRole)
+
+                if (!roles.includes(payload.role_name)) {
+                    return res.status(403).json({
+                        success: false,
+                        message: "Forbidden: insufficient role",
+                        result: null
+                    })
+                }
             }
 
             next()
